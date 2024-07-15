271384ec2de0609cecb1d67092694e556ee20b9c
jayavarapu25 <66918446+jayavarapu25@users.noreply.github.com>
2024-07-14 22:25:20 -0500

    first commit

---
+++ Devops-Example2/src/main/java/com/example/DevopsExample2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsExample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsExample2Application.class, args);
+	}
+
+}
